8e6dfcbc9bfcdb193b3d0a3778f74f709dfaba3f
Chairal <chairal21001@mail.unpad.ac.id>
2024-06-04 02:28:51 +0700

    add Product Perlengkapan Kantor Page

---
+++ src/app/product-perlengkapan-kantor/page.tsx
@@ -0,0 +1,11 @@
+import { PerlengkapanKantorCarousel } from "@/components/PerlengkapanKantorCarousel";
+import { PerlengkapanKantorProduct } from "@/components/PerlengkapanKantorProduct";
+
+export default function ProductPerlengkapanKantorPage() {
+  return (
+    <main className="">
+      <PerlengkapanKantorCarousel />
+      <PerlengkapanKantorProduct />
+    </main>
+  );
+}
---
+++ src/components/PerlengkapanKantorProduct.tsx
@@ -0,0 +1,174 @@
+import * as React from "react";
+
+import {
+  Card,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function PerlengkapanKantorProduct() {
+  return (
+    <>
+      <div className="text-center mt-[75px] mb-[25px]">
+        <h4 className="text-[#3E4095] text-[28px] font-bold">Produk Kami</h4>
+      </div>
+      <div className="grid grid-cols-4 gap-[30px] mb-[87px] w-[1241px] mx-auto">
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct1.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Buku Agenda
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct2.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Kartu Ucapan
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct3.png"
+                alt=""
+                className="w-[263px] h-[200px] top-10 rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            ID Card
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct4.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Amplop
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct5.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Angpao
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct6.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Brosur
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct7.png"
+                alt=""
+                className="w-[263px] h-[200px] top-10 rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Kop Surat
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct8.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover object-bottom"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Lanyard
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct9.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Kartu Nama
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct10.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Map Folder
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct11.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Kartu Member
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct12.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Kalender
+          </CardTitle>
+        </Card>
+        <Card className="w-[283px] h-[263px] rounded-[10px] bg-[#FFFFFF] border-[0.5px] flex flex-col">
+          <div className="px-[10px] py-[10px]">
+            <img
+                src="./PerlengkapanKantorProduct13.png"
+                alt=""
+                className="w-[263px] h-[200px] rounded-[5px] object-cover"
+              />
+          </div>
+          <CardTitle className="text-[16px] text-[#3E3E3E] font-semibold text-center px-[10px] pt-[5px] pb-[15px]">
+            Tiket
+          </CardTitle>
+        </Card>
+      </div>
+    </>
+  );
+}
